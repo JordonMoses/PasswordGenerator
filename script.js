c1974db32b68d77eb437b69340bcf9855bdca993
JordonMoses <jordon@blackbirdrevolt.com>
2020-10-27 16:59:33 -0500

    Fixed error is js

--- script.js
+++ script.js
@@ -25,68 +25,65 @@ function generatePassword() {
   masterArray = []
   let charNum = prompt("How many characters in your password?");
   console.log(charNum)
-// Loops if characters are les than 8 and more than 128 (while loop not working until if and break statements added)
-  while(charNum < 8 || charNum > 128 || isNaN(charNum)) {
+  // Loops if characters are les than 8 and more than 128 (while loop not working until if and break statements added)
+  while (charNum < 8 || charNum > 128 || isNaN(charNum)) {
     alert("Password length must be 8-128 characters(enter a number)");
-    charNum = prompt("How many characters in your password?");{ // Can still type text and move forward
+    charNum = prompt("How many characters in your password?"); { // Can still type text and move forward
     }
   }
 
-// Password character types
+  // Password character types
   let lower = confirm("Do you want lowercase letters?");
   let upper = confirm("Do you want uppercase letters?");
   let numbers = confirm("Do you want numbers?");
   let char = confirm("Do you want special characters?");
 
-// Loops if user selects no character type ()
+  // Loops if user selects no character type ()
   // if(lower === false && upper === false && numbers === false && char === false) {
   //   alert("Password must include at least one character type");
   //   confirm("Do you want lowercase letters?");
   //   confirm("Do you want uppercase letters?");
   //   confirm("Do you want numbers?");
   //   confirm("Do you want special characters?");
-  } 
-  
-
-  
-
-    if (lower === true){
-      masterArray.push(lowercase)
-    }
-  
-    if (upper === true){
-      masterArray.push(uppercase)
-    }
-  
-    if (numbers === true){
-      masterArray.push(numeric)
-    }
-  
-    if (char === true){
-      masterArray.push(specialChar)
-    } 
-  console.log(masterArray, "THIS IS THE MASTER ARRAY")
-
-
-  let arrayIntoString = masterArray.join("").split(",")
-  console.log(arrayIntoString)
-
-  let finalPassword = []
-
-  for(let i=0; i<charNum; i++){
-    let passwordCharacter = getRandom(arrayIntoString)
-    finalPassword.push(passwordCharacter)
-
-  }
-  console.log(finalPassword);
 
 
 
-  return finalPassword.join("");
+
+if (lower === true) {
+  masterArray.push(lowercase)
+}
+
+if (upper === true) {
+  masterArray.push(uppercase)
+}
+
+if (numbers === true) {
+  masterArray.push(numeric)
+}
+
+if (char === true) {
+  masterArray.push(specialChar)
+}
+console.log(masterArray, "THIS IS THE MASTER ARRAY")
+
+
+let arrayIntoString = masterArray.join("").split(",")
+console.log(arrayIntoString)
+
+let finalPassword = []
+
+for (let i = 0; i < charNum; i++) {
+  let passwordCharacter = getRandom(arrayIntoString)
+  finalPassword.push(passwordCharacter)
+
+}
+console.log(finalPassword);
+
+return finalPassword.join("");
 
 }
 
-function getRandom(arr){
+function getRandom(arr) {
   let randomCharacter = Math.floor(Math.random() * arr.length)
   let randomIndex = arr[randomCharacter]
   return randomIndex
